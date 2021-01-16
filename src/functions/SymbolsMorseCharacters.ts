82d7a520f222c8670419bd5d14a7eb2a80bb40af
nightlyds <safonov.danil.cw@gmail.com>
2021-01-16 15:54:54 +0200

    Duplicate identifier Typescript SymbolsMorseCharacters file

--- src/functions/SymbolsMorseCharacters.ts
+++ src/functions/SymbolsMorseCharacters.ts
@@ -21,7 +21,7 @@ const SymbolsMorseCharacters: Record<string, string> = {
     "(": "-.--.",
     ")": "-.--.-",
     "&": ".-...",
-    ":": "-.-.-.", 
+    // ":": "-.-.-.",  Typescript issue: 'Duplicate identifier'
     "+": ".-.-.",
     "-": "-....-",
     _: "..--.-",
